cab191927a57f72fe561f61c756b955dc65cd892
Viincii <vinc.mignot@hotmail.fr>
2021-01-20 18:49:20 +0100

    commit on devVince

--- Composants/CompConnexion/VueComposantConnexion.php
+++ Composants/CompConnexion/VueComposantConnexion.php
@@ -5,7 +5,7 @@
 			$this->menu = ((!isset($_SESSION['id'])) ?
 			 (($_GET['action'] != 'bienvenue' && $_GET['action'] != 'connecte' && $_GET['action'] != 'initIns') 
 			 ?"<div id='miniProfil'><a href=index.php?module=connexion&action=bienvenue><button class='butP' id='butConn'>Connexion</button></a></br></div>" : '') 
-			 :(($_GET['module']!='profil')?"<div id='miniProfil'><a href=index.php?module=profil><img src=".$_SESSION['avatar']." id=miniAvat><div id='miniPseudo'>".$_SESSION['id']."</div></a></br>"."<a href=index.php?module=connexion&action=deco><button class='butP'>Deconnexion</button></a></br></div>":''));
+			 :(($_GET['module']!='profil')?"<div id='miniProfil'><a href=index.php?module=profil><img src=".$_SESSION['avatar']." id=miniAvat><div id='miniPseudo'>".$_SESSION['id']."</div></a></br>"."<a href=index.php?module=connexion&action=deco><button class='butP'>Déconnexion</button></a></br></div>":''));
 		}
 
 		function affiche(){
--- Composants/CompMenu/VueComposantMenu.php
+++ Composants/CompMenu/VueComposantMenu.php
@@ -2,13 +2,13 @@
 	class VueComposantMenu{
 		private $menu;
 		function __construct(){
-			$this->menu = "<a href=index.php?module=Lore><img class='menu' src='Image/lore.png' alt=''>
+			$this->menu = "<div id='blockMenu'><a href=index.php?module=Lore><img class='menu' src='Image/lore.png' alt=''>
 			</a><a href=index.php?module=Bestiaire><img class='menu'src='Image/bestiaire.png' alt=''</a>
 			<a href=index.php?module=Deck><img class='menu' src='Image/Deck.png' alt=''</a>
 			<a href=index.php?module=Jouer><img class='menu' src='Image/Jouer.png' alt=''</a>
 			<a href=index.php?module=Ligue><img class='menu' src='Image/ligue.png' alt=''</a>
 			<a href=index.php?module=Guilde><img class='menu' src='Image/guilde.png' alt=''</a>
-			<a href=index.php?module=Patchs><img class='menu' src='Image/Patch.png' alt=''></a>";
+			<a href=index.php?module=Patchs><img class='menu' src='Image/Patch.png' alt=''></a></div>";
 		}
 
 		function affiche(){
